47b50c0f90b364bd4db8d6721dcd3daa8bdb976c
ginle20799 <leminhnhat20799@gmail.com>
2018-12-08 18:38:50 +0700

    update layout4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,30 @@
 // Main
-$(document).ready(function () {
-	// Phần Slider
-	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2,
-			},
-			// breakpoint from 768 up
-			768: {
-				items: 4,
-			},
-			// breakpoint from 992 up
-			992: {
-				items: 6,
-			}
-		}
-	});
+$(document).ready(function() {
+    // Phần Slider
+    $('.home-slider .owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    })
+
+    // Phần Feedback
+    $('.home-feedback .owl-carousel').owlCarousel({
+        items: 1,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 250,
+    })
+
+    // Phần Client
+    $('.home-client .owl-carousel').owlCarousel({
+        items: 6,
+        nav: true,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 250,
+        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    })
 });
--- tasks/copy-fonts.js
+++ tasks/copy-fonts.js
@@ -1,8 +1,8 @@
-module.exports = function (gulp, $, browserSync) {
-	gulp.task('copy-fonts', function () {
-		return gulp.src([
-				'./src/fonts/**.*',
-			])
-			.pipe(gulp.dest('./dist/fonts'));
-	});
+module.exports = function(gulp, $, browserSync) {
+    gulp.task('copy-fonts', function() {
+        return gulp.src([
+                './src/webfonts/**.*',
+            ])
+            .pipe(gulp.dest('./dist/webfonts'));
+    });
 };
